ashishb/wp2hugo

commit cb398656686bab5e7427b84012c8bf42a4e49bc9

fix: remove more invalid XML characters
Ref: https://github.com/ashishb/wp2hugo/issues/27

src/wp2hugo/internal/wpparser/invalid_character_remover.go
@@ -15,7 +15,12 @@ func (i InvalidatorCharacterRemover) Read(p []byte) (int, error) {
 	if err != nil {
 		return n, err
 	}
-	tmp = bytes.ReplaceAll(tmp, []byte("\u000C"), []byte(""))
+	// Characters from 1 to 31 seem to be disallowed in XML
+	// One gets errors like "XML syntax error on line <>: illegal character code U+0001"
+	// Ref: https://github.com/ashishb/wp2hugo/issues/27
+	for i := 0; i <= 31; i++ {
+		tmp = bytes.ReplaceAll(tmp, []byte{byte(i)}, []byte(""))
+	}
 	copy(p, tmp)
 	return len(tmp), nil
 }

src/wp2hugo/internal/wpparser/wp_parser_setup.go
@@ -153,6 +153,9 @@ func (p *Parser) Parse(xmlData io.Reader) (*WebsiteInfo, error) {
 	fp := rss.Parser{}
 	feed, err := fp.Parse(InvalidatorCharacterRemover{reader: xmlData})
 	if err != nil {
+		log.Warn().
+			Err(err).
+			Msgf("error parsing XML")
 		return nil, fmt.Errorf("error parsing XML: %s", err)
 	}
 	return p.getWebsiteInfo(feed)

commit f523cd3574b44321845c35df41dc8148025f67ad

fix: don't remove \n, \r, or \t characters from XML
This bug was introduced in cb398656686bab5e7427b84012c8bf42a4e49bc9

src/wp2hugo/internal/wpparser/invalid_character_remover.go
@@ -17,8 +17,14 @@ func (i InvalidatorCharacterRemover) Read(p []byte) (int, error) {
 	}
 	// Characters from 1 to 31 seem to be disallowed in XML
 	// One gets errors like "XML syntax error on line <>: illegal character code U+0001"
-	// Ref: https://github.com/ashishb/wp2hugo/issues/27
+	// Ref:
+	// 1. https://github.com/ashishb/wp2hugo/issues/27
+	// 2. https://www.ascii-code.com/characters/control-characters
 	for i := 0; i <= 31; i++ {
+		// Don't remove CR or LF
+		if byte(i) == '\n' || byte(i) == '\r' || byte(i) == '\t' {
+			continue
+		}
 		tmp = bytes.ReplaceAll(tmp, []byte{byte(i)}, []byte(""))
 	}
 	copy(p, tmp)
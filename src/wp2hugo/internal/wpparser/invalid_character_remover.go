f523cd3574b44321845c35df41dc8148025f67ad
Ashish Bhatia <ashishb@ashishb.net>
2024-08-17 19:08:38 -0700

    fix: don't remove \n, \r, or \t characters from XML
    This bug was introduced in cb398656686bab5e7427b84012c8bf42a4e49bc9

--- src/wp2hugo/internal/wpparser/invalid_character_remover.go
+++ src/wp2hugo/internal/wpparser/invalid_character_remover.go
@@ -17,8 +17,14 @@ func (i InvalidatorCharacterRemover) Read(p []byte) (int, error) {
 	}
 	// Characters from 1 to 31 seem to be disallowed in XML
 	// One gets errors like "XML syntax error on line <>: illegal character code U+0001"
-	// Ref: https://github.com/ashishb/wp2hugo/issues/27
+	// Ref:
+	// 1. https://github.com/ashishb/wp2hugo/issues/27
+	// 2. https://www.ascii-code.com/characters/control-characters
 	for i := 0; i <= 31; i++ {
+		// Don't remove CR or LF
+		if byte(i) == '\n' || byte(i) == '\r' || byte(i) == '\t' {
+			continue
+		}
 		tmp = bytes.ReplaceAll(tmp, []byte{byte(i)}, []byte(""))
 	}
 	copy(p, tmp)
