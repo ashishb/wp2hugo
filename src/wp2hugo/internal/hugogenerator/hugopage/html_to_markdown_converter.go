6d4a65a2fac1e52292d1d5a8af8fe86a30349ed0
Lawrence Gripper <lawrencegripper@github.com>
2024-09-04 22:06:47 +0100

    Add a converter that finds gist URLs and converts these to gist shortcodes for Hugo (#39)
    * Add parsing for gist urls to gist shortcodes
    
    * Handle inline markdown gist syntax too
    
    * Update naming
    
    * Pull out common code
    
    * Ensure block parsing doesn't break image parsing
    
    * Handle stripping `?w=1024` from images added in the block editor
    
    These break the hugo serve and embedding.
    
    * Fix up linting issues

--- src/wp2hugo/internal/hugogenerator/hugopage/html_to_markdown_converter.go
+++ src/wp2hugo/internal/hugogenerator/hugopage/html_to_markdown_converter.go
@@ -2,15 +2,18 @@ package hugopage
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
+
 	md "github.com/JohannesKaufmann/html-to-markdown"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/rs/zerolog/log"
-	"regexp"
-	"strings"
 )
 
 var youtubeID = regexp.MustCompile(`youtube\.com/embed/([^\&\?\/]+)`)
 var googleMapsID = regexp.MustCompile(`google\.com/maps/d/.*embed\?mid=([0-9A-Za-z-_]+)`)
+var gistUrl = regexp.MustCompile(`gist\.github\.com/([^/]+)/([0-9a-f]+)`)
+var gistMarkdown = regexp.MustCompile(`\\\[gist .*\]`)
 
 func getMarkdownConverter() *md.Converter {
 	converter := md.NewConverter("", true, nil)
@@ -18,6 +21,8 @@ func getMarkdownConverter() *md.Converter {
 	converter.Use(getGoogleMapsEmbedForHugoConverter())
 	converter.Use(convertCustomBRToNewline())
 	converter.Use(convertBrToNewline())
+	converter.Use(convertGistURLsToShortcodes())
+	converter.Use(handleBlockEditorImages())
 	return converter
 }
 
@@ -69,6 +74,78 @@ func getGoogleMapsEmbedForHugoConverter() md.Plugin {
 						Str("id", id).
 						Msg("Google Maps embed found")
 					text := fmt.Sprintf("{{< googlemaps src=\"%s\" width=%s height=%s >}}", id, width, height)
+					return &text
+				},
+			},
+		}
+	}
+}
+
+func extractShortcodeFromGistUrl(content string) string {
+	parts := gistUrl.FindStringSubmatch(content)
+	if len(parts) != 3 {
+		return content
+	}
+	user := parts[1]
+	id := parts[2]
+	text := fmt.Sprintf("{{< gist %s %s >}}", user, id)
+	log.Debug().
+		Str("user", user).
+		Str("id", id).
+		Msg("Gist URL found")
+	return text
+}
+
+func convertGistURLsToShortcodes() md.Plugin {
+	return func(c *md.Converter) []md.Rule {
+		return []md.Rule{
+			// Handle new embed style from block editor
+			{
+				Filter: []string{"figure"},
+				Replacement: func(content string, selec *goquery.Selection, opt *md.Options) *string {
+					classes := selec.AttrOr("class", "")
+					if !strings.Contains(classes, "is-provider-embed-handler") {
+						return &content
+					}
+					shortcode := extractShortcodeFromGistUrl(content)
+					return &shortcode
+				},
+			},
+			// Handle basic `[gist url] embed`
+			{
+				Filter: []string{"body"},
+				Replacement: func(content string, selec *goquery.Selection, opt *md.Options) *string {
+					text := gistMarkdown.ReplaceAllStringFunc(content, func(s string) string {
+						return extractShortcodeFromGistUrl(content)
+					})
+					return &text
+				},
+			},
+		}
+	}
+}
+
+// <figure class="wp-block-image size-large"><a href="https://blog.gripdev.xyz/wp-content/uploads/2024/03/image.png"><img src="https://blog.gripdev.xyz/wp-content/uploads/2024/03/image.png?w=1024" alt="" class="wp-image-1663" /></a></figure>
+func handleBlockEditorImages() md.Plugin {
+	return func(c *md.Converter) []md.Rule {
+		return []md.Rule{
+			{
+				Filter: []string{"figure"},
+				Replacement: func(content string, selec *goquery.Selection, opt *md.Options) *string {
+					classes := selec.AttrOr("class", "")
+					if !strings.Contains(classes, "wp-block-image") {
+						return &content
+					}
+					imgUrl := selec.Find("figure.wp-block-image a img").First()
+					imgSrc := imgUrl.AttrOr("src", "")
+					if imgSrc == "" {
+						return &content
+					}
+					strippedImgSrc := strings.Split(imgSrc, "?")[0]
+					text := strings.Replace(content, imgSrc, strippedImgSrc, 1)
+					log.Debug().
+						Str("imgSrc", imgSrc).
+						Msg("Block editor image found, stripping params")
 					return &text
 				},
 			},
--- src/wp2hugo/internal/hugogenerator/hugopage/html_to_markdown_converter_test.go
+++ src/wp2hugo/internal/hugogenerator/hugopage/html_to_markdown_converter_test.go
@@ -1,12 +1,31 @@
 package hugopage
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 const _textWithIframe = `
 <p><iframe src="https://www.google.com/maps/d/u/0/embed?mid=1lcjyzfxxXcdDP3XkrikfqIJryfFi4ZA&amp;ehbc=2E312F" width="640" height="480"></iframe></p>
+`
+
+const _textWithBlockGist = `
+<figure class="wp-block-embed is-type-rich is-provider-embed-handler wp-block-embed-embed-handler"><div class="wp-block-embed__wrapper">
+https://gist.github.com/lawrencegripper/8e701b0d201e65af0f8bc9b8b0b14207
+</div></figure>
+`
+
+const _textWithImgFigureBlock = `
+<figure class="wp-block-image size-large"><a href="https://blog.gripdev.xyz/wp-content/uploads/2024/03/image.png"><img src="https://blog.gripdev.xyz/wp-content/uploads/2024/03/image.png?w=1024" alt="" class="wp-image-1663" /></a></figure>
+`
+
+const _textMarkdownGist = `
+<body>
+some text
+\[gist https://gist.github.com/lawrencegripper/6bee7de123bea1936359\]
+some more text
+</body>
 `
 
 func TestIframe(t *testing.T) {
@@ -14,4 +33,32 @@ func TestIframe(t *testing.T) {
 	result, err := converter.ConvertString(_textWithIframe)
 	assert.NoError(t, err)
 	assert.Contains(t, result, `{{< googlemaps src="1lcjyzfxxXcdDP3XkrikfqIJryfFi4ZA" width=640 height=480 >}}`)
+}
+
+func TestBlockGist(t *testing.T) {
+	converter := getMarkdownConverter()
+	result, err := converter.ConvertString(_textWithBlockGist)
+	assert.NoError(t, err)
+	assert.Contains(t, result, `{{< gist lawrencegripper 8e701b0d201e65af0f8bc9b8b0b14207 >}}`)
+}
+
+func TestBlockGistDoesNotBreakImgParsing(t *testing.T) {
+	converter := getMarkdownConverter()
+	result, err := converter.ConvertString(_textWithImgFigureBlock)
+	assert.NoError(t, err)
+	assert.Equal(t, result, `[![](https://blog.gripdev.xyz/wp-content/uploads/2024/03/image.png?w=1024)](https://blog.gripdev.xyz/wp-content/uploads/2024/03/image.png)`)
+}
+
+func TestBlockEditorImgExtractedWithoutWidthParam(t *testing.T) {
+	converter := getMarkdownConverter()
+	result, err := converter.ConvertString(_textWithImgFigureBlock)
+	assert.NoError(t, err)
+	assert.Equal(t, result, `[![](https://blog.gripdev.xyz/wp-content/uploads/2024/03/image.png)](https://blog.gripdev.xyz/wp-content/uploads/2024/03/image.png)`)
+}
+
+func TestMarkdownGist(t *testing.T) {
+	converter := getMarkdownConverter()
+	result, err := converter.ConvertString(_textMarkdownGist)
+	assert.NoError(t, err)
+	assert.Contains(t, result, `{{< gist lawrencegripper 6bee7de123bea1936359 >}}`)
 }
