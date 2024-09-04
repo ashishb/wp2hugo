f29bccf48af775e9d4015c9ef93d2a1ee6dec6d3
lawrencegripper <info@grippers.co.uk>
2024-09-04 20:21:46 +0100

    Update naming

--- src/wp2hugo/internal/hugogenerator/hugopage/html_to_markdown_converter_test.go
+++ src/wp2hugo/internal/hugogenerator/hugopage/html_to_markdown_converter_test.go
@@ -10,7 +10,7 @@ const _textWithIframe = `
 <p><iframe src="https://www.google.com/maps/d/u/0/embed?mid=1lcjyzfxxXcdDP3XkrikfqIJryfFi4ZA&amp;ehbc=2E312F" width="640" height="480"></iframe></p>
 `
 
-const _textWithGist = `
+const _textWithBlockGist = `
 <figure class="wp-block-embed is-type-rich is-provider-embed-handler wp-block-embed-embed-handler"><div class="wp-block-embed__wrapper">
 https://gist.github.com/lawrencegripper/8e701b0d201e65af0f8bc9b8b0b14207
 </div></figure>
@@ -31,9 +31,9 @@ func TestIframe(t *testing.T) {
 	assert.Contains(t, result, `{{< googlemaps src="1lcjyzfxxXcdDP3XkrikfqIJryfFi4ZA" width=640 height=480 >}}`)
 }
 
-func TestGist(t *testing.T) {
+func TestBlockGist(t *testing.T) {
 	converter := getMarkdownConverter()
-	result, err := converter.ConvertString(_textWithGist)
+	result, err := converter.ConvertString(_textWithBlockGist)
 	assert.NoError(t, err)
 	assert.Contains(t, result, `{{< gist lawrencegripper 8e701b0d201e65af0f8bc9b8b0b14207 >}}`)
 }
