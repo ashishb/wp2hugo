13578a594cc1887103454f084b9639044c994a0e
Aurélien PIERRE <aurelienpierre@users.noreply.github.com>
2025-07-06 06:48:02 +0200

    Support Gutenberg blocks and convert to Hugo shortcodes (#184)
    * Support Youtube with [embed] WP shortcode
    
    * Relax the [caption] handling regex
    
    Some `<img>` don't have `class=" "`
    
    * Gutenberg figure element: replace with Hugo figure shortcode
    
    Improve caption regex too.
    
    * Youtube replacement: fix a bug, support Gutenberg Youtube block
    
    * Support Gutenberg gallery blocks
    
    * Document shortcodes replacements
    
    * Revert to string ID
    
    * Forgotten MD extension
    
    * Check HTML returned error
    
    * Fix MD syntax

--- src/wp2hugo/internal/hugogenerator/hugopage/wordpress_youtube_converter.go
+++ src/wp2hugo/internal/hugogenerator/hugopage/wordpress_youtube_converter.go
@@ -10,12 +10,23 @@ import (
 // Example: Plain-text Youtube URLs on their own line in post content are turned by WP into embeds
 // The YouTube Lyte plug-in additionally uses "httpa://" for audio and "httpv://" for video embeds:
 // https://wordpress.com/plugins/wp-youtube-lyte
-var _YoutubeRegEx = regexp.MustCompile(`(?m)(^|\s)(?:[embed])http[sav]?://(?:m\.|www\.)?(?:youtu\.be|youtube\.com)/(?:watch|w)\?v=([^&\s]+)(?:[/embed])`)
+var _YoutubeRegEx = regexp.MustCompile(`(?m)(^|\s)(?:\[embed\])?http[sav]?://(?:m\.|www\.)?(?:youtu\.be|youtube\.com)/(?:watch|w)\?v=([^&\s\[\]]+)(?:\[\/embed\])?`)
+
+// Gutenberg Youtube embed into figure:
+// <!-- wp:embed {"url":"https://www.youtube.com/watch?v=7l6FjphZXsk","type":"video","providerNameSlug":"youtube","responsive":true,"align":"full","className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+// <figure class="wp-block-embed alignfull is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+// https://www.youtube.com/watch?v=7l6FjphZXsk
+// </div></figure>
+// <!-- /wp:embed -->
+var _YoutubeGutenbergRegEx = regexp.MustCompile(`(?ms)(^|\s)<!-- wp:embed {"url":"[^"]+v=([^"]+)".*?<!-- /wp:embed -->`)
 
 func replacePlaintextYoutubeURL(htmlData string) string {
 	log.Debug().
 		Msg("Replacing Youtube URLs with embeds")
-	return replaceAllStringSubmatchFunc(_YoutubeRegEx, htmlData, YoutubeReplacementFunction)
+
+	htmlData = replaceAllStringSubmatchFunc(_YoutubeGutenbergRegEx, htmlData, YoutubeReplacementFunction)
+	htmlData = replaceAllStringSubmatchFunc(_YoutubeRegEx, htmlData, YoutubeReplacementFunction)
+	return htmlData
 }
 
 func YoutubeReplacementFunction(groups []string) string {
